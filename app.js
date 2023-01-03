9673bf65eb9d44d1e067ff98a5794bf95b0c4dbf
AMELIE GUILLOUX <amelie.guilloux@hotmail.fr>
2023-01-03 10:58:21 +0100

    sessions

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
-
 const express = require("express");
+const app = express();
+app.use(express.json());
+const Session = require('express-session');
+const path = require('path');
+const FileStore = require('session-file-store')(Session);
 const { hashPassword, verifyPassword, verifyToken, verifyTokenById } = require("./auth");
 
-const app = express();
-
-app.use(express.json());
-
 const port = process.env.APP_PORT ?? 5000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -15,6 +23,25 @@ const welcome = (req, res) => {
 
 const userHandlers = require("./userHandlers");
 const movieHandlers = require("./movieHandlers");
+
+app.use(Session({
+  store: new FileStore({
+    path: path.join(__dirname, '/tmp'),
+    encrypt: true
+  }),
+  secret: 'Super Secret !',
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId'
+}));
+
+app.get("/session-in", (req, res) => {
+  req.session.song = "be bop a lula";
+  res.end();
+});
+app.get("/session-out", (req, res) => {
+  res.send(req.session.song);
+});
 
 app.get("/", welcome);
 app.get("/api/users", userHandlers.getUsers);
@@ -36,13 +63,5 @@ app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.put("/api/users/:id", verifyTokenById, userHandlers.updateUser);
 app.delete("/api/users/:id", verifyTokenById, userHandlers.deleteUser);
-
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
 
 
